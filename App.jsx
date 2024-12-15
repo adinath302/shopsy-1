30675728d45719c1182c5e779b8df801a625201a
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2024-12-15 22:29:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+// import App from './App.css'
+
+const App = () => {
+  return (
+    <div className="">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
